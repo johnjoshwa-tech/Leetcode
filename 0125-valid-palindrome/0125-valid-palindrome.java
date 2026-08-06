02e7aee3dc702222f8d285755433b5d69dd05f93
johnjoshwa-tech <johnjoshwa.tech@gmail.com>
2026-08-06 17:57:37 +0530

    Time: 3 ms (58.3%), Space: 44.4 MB (55.4%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        StringBuilder str = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if ((ch >= 'a' && ch <= 'z') ||
+                (ch >= 'A' && ch <= 'Z') ||
+                (ch >= '0' && ch <= '9')) {
+
+                str.append(Character.toLowerCase(ch));
+            }
+        }
+
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right))
+                return false;
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
